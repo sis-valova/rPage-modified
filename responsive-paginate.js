efeb85de0eefe4e4528386777bcfd014844d3875
NIS <nis@NISs-iMac.local>
2017-07-08 23:15:18 +0300

    Rewriting some parts

--- responsive-paginate.js
+++ responsive-paginate.js
@@ -1,101 +1,85 @@
 /*
  * A plugin for making Bootstrap's pagination more responsive
  * https://github.com/auxiliary/rpage
+ *
+ *
  */
 
-(function ($){
+(function ($) {
     jQuery.fn.rPage = function (options) {
+
         var $this = $(this);
-        
+
         var settings = $.extend({
-            gap: 100,
+            gap: 10,
             resize_time: 20
-        }, options );
-        
+        }, options);
+
+        var instances = [];
+
         for(var i = 0, max = $this.length; i < max; i++)
         {
-        	new rPage($($this[i]));
+            instances.push(new rPage($($this[i])).label().makeResponsive());
         }
+
+        var resize_timer;
+
+        $(window).on('resize', function () {
+            clearTimeout(resize_timer);
+            resize_timer = setTimeout(function () {
+            	for(var i = 0, len = instances.length; i < len; i++) {
+                    instances[i].makeResponsive();
+				}
+            }, settings.resize_time)
+        });
 
         function rPage($container)
         {
-        	this.label = function()
-        	{
-        		var active_index = this.els.filter(".active").index();
-        		var rp = this;
-        		this.els.each(function(){
-        			if (rp.isNextOrPrevLink($(this)) == false)
-        			{
-        				$(this).addClass("page-away-" + (Math.abs(active_index - $(this).index())).toString());
-        			}
-        			else
-        			{
-        				if ($(this).index() > active_index)
-        				{
-        					$(this).addClass("right-etc");
-        				}
-        				else
-        				{
-        					$(this).addClass("left-etc");
-        				}
-        			}
+            var self = this;
+
+            self.els = $container.find("li");
+
+            self.label = function() {
+        		var active_index = self.els.filter(".active").index();
+
+                self.els.each(function() {
+                    $(this).addClass("page-away-" + (Math.abs(active_index - $(this).index())));
         		});
-        	}
 
-        	this.makeResponsive = function()
-    	    {
-    	    	this.reset();
-    	    	var width = this.calculateWidth();
+                return self;
+        	};
 
-    	    	while (width > this.els.parent().parent().outerWidth() - settings.gap)
-    	    	{
-    	    		var did_remove = this.removeOne();
-    	    		if (did_remove == false)
-    	    		{
-    	    			break;
-    	    		}
-    	    		width = this.calculateWidth();
-    	    	}
-    	    }
+            self.getNextPrevWidth = function() {
+            	var prev = $container.parent().find('.prev a').outerWidth(true);
+            	var next = $container.parent().find('.next a').outerWidth(true);
+            	return prev + next + settings.gap;
+			};
+            self.makeResponsive = function() {
+                self.reset();
+    	    	var width = $container.outerWidth(true);
+    	    	var outer_container = self.els.parent().parent().width() - self.getNextPrevWidth();
 
-        	this.isNextOrPrevLink = function(element)
-        	{
-                return (
-                    element.hasClass('pagination-prev')
-                    || element.hasClass('pagination-next')
-                    || element.text() == "»"
-                    || element.text() == "«"
-                );
-        	}
+    	    	while (width > outer_container) {
+    	    		if (!self.removeOne()) break;
+    	    		width = $container.outerWidth(true);
+                
+                }
 
-        	this.isRemovable = function(element)
-        	{
-        		if (this.isNextOrPrevLink(element))
-        		{
-        			return false;
-        		}
-        		var index = this.els.filter(element).index();
-        		if (index == 1 || this.isNextOrPrevLink($container.find("li").eq(index + 1)))
-        		{
-        			return false;
-        		}
-        		if (element.text() == "...")
-        		{
-        			return false;
-        		}
-        		return true;
-        	}
+                return self;
+    	    };
 
-    	    this.removeOne = function()
-    	    {
-    	    	var active_index = this.els.filter(".active").index();
+            self.isRemovable = function(element) {
+        		var index = self.els.filter(element).index();
+        		return !(index === 0 || index === ($container.find("li").length - 1) || element.text() === "...");
+        	};
+
+            self.removeOne = function() {
+    	    	var active_index = self.els.filter(".active").index();
     	    	var farthest_index = $container.find("li").length - 1;
-    	    	var next = active_index + 1;
-    	    	var prev = active_index - 1;
 
     	    	for (var i = farthest_index - 1; i > 0; i--)
     	    	{
-    	    		var candidates = this.els.filter(".page-away-" + i.toString());
+    	    		var candidates = self.els.filter(".page-away-" + i);
     	    		var candidate = candidates.filter(function(){
     	    			return this.style["display"] != "none";
     	    		});
@@ -104,16 +88,16 @@
     	    			for (var j = 0; j < candidate.length; j++)
     	    			{
     	    				var candid_candidate = candidate.eq(j);
-    	    				if (this.isRemovable(candid_candidate))
+    	    				if (self.isRemovable(candid_candidate))
     		    			{
-    			    			candid_candidate.css("display", "none");
-    			    			if (this.needsEtcSign(active_index, farthest_index - 1))
+    			    			candid_candidate.css("display", "none").removeClass('is-inline').addClass('is-hidden');
+    			    			if (self.needsEtcSign(active_index, farthest_index ))
     			    			{
-    			    				this.els.eq(farthest_index - 2).before("<li class='disabled removable'><span>...</span></li>");
+                                    self.els.eq(farthest_index - 2).before("<li class='disabled removable'><span>...</span></li>");
     			    			}
-    			    			if (this.needsEtcSign(1, active_index))
+    			    			if (self.needsEtcSign(0, active_index))
     			    			{
-    			    				this.els.eq(1).after("<li class='disabled removable'><span>...</span></li>");
+                                    self.els.eq(1).after("<li class='disabled removable'><span>...</span></li>");
     			    			}
     			    			return true;
     		    			}
@@ -121,10 +105,9 @@
     	    		}
     	    	}
     	    	return false;
-    	    }
+    	    };
 
-    	    this.needsEtcSign = function(el1_index, el2_index)
-    	    {
+            self.needsEtcSign = function(el1_index, el2_index) {
     	    	if (el2_index - el1_index <= 1)
     	    	{
     	    		return false;
@@ -151,41 +134,16 @@
     	    		}
     	    	}
     	    	return false;
-    	    }
+    	    };
 
-    	    this.reset = function()
-    	    {
-    	    	for (var i = 0; i < this.els.length; i++)
+            self.reset = function() {
+    	    	for (var i = 0; i < self.els.length; i++)
     	    	{
-    	    		this.els.eq(i).css("display", "inline");
+                    self.els.eq(i).css("display", "inline").removeClass('is-hidden').addClass('is-inline');
     	    	}
-    	    	$container.find("li").filter(".removable").remove();
-    	    }
+    	    	$container.find("li.removable").remove();
+    	    };
 
-    	    this.calculateWidth = function()
-    	    {
-    	    	var width = 0;
-    	    	for (var i = 0; i < $container.find("li").length; i++)
-    	    	{
-    	    		width += $container.find("li").eq(i).children("a").eq(0).outerWidth();
-    	    		width += $container.find("li").eq(i).children("span").eq(0).outerWidth();
-    	    	}
-    	    	return width;
-    	    }
-
-    	    this.els = $container.find("li");
-    	    this.label();
-    	    this.makeResponsive();
-
-    	    var resize_timer;
-
-            $(window).resize(
-            	$.proxy(function()
-            	{
-            		clearTimeout(resize_timer);
-            		resize_timer = setTimeout($.proxy(function(){this.makeResponsive()}, this), settings.resize_time);
-            	}, this)
-            );
         }
     };
 }(jQuery));
